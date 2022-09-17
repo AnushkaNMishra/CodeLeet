0cd1d915069c7be10c5192062c78948b7cf861fd
Anushka N Mishra <84915436+AnushkaNMishra@users.noreply.github.com>
2022-09-18 03:03:02 +0530

    Time: 4 ms (71.85%), Space: 5.7 MB (87.59%) - LeetHub

--- 507-perfect-number/507-perfect-number.cpp
+++ 507-perfect-number/507-perfect-number.cpp
@@ -5,19 +5,17 @@ public:
         int temp=0;
     for(int i=1;i*i<num;i++){
         if(num%i==0){
-            cout<<i<<"a ";
             temp+=i;
         if(i!=num/i){
             int a=num/i;
-                cout<<num/i<<"b ";
                 temp+=a;
             }
             
         }
         
-    }cout<<temp<<"a";
-        int res=temp-x;
-        if(res==x)return true;
+    }
+        
+        if((temp-x)==x)return true;
         return false;
     }
 };
