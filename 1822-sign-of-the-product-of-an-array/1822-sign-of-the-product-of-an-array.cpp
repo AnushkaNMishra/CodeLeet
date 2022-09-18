d622b3710cb787faef8f0b2951c343a86cf93924
Anushka N Mishra <84915436+AnushkaNMishra@users.noreply.github.com>
2022-09-18 12:42:36 +0530

    Time: 11 ms (47.39%), Space: 10.3 MB (43.03%) - LeetHub

---
+++ 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int arraySign(vector<int>& nums) {
+        float product=1;
+        for(int i=0;i<nums.size();i++){
+            product=product*nums[i];
+        }
+        if(product>0)
+            return 1;
+        else if(product<0)
+            return -1;
+        else 
+            return 0;
+    }
+};
