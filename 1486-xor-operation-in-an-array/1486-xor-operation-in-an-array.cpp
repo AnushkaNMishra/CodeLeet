34505fd6da59bf741b69d7cb93c52bc575010076
Anushka N Mishra <84915436+AnushkaNMishra@users.noreply.github.com>
2022-09-25 03:29:43 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (79.76%) - LeetHub

--- 1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.cpp
+++ 1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int xorOperation(int n, int start) {
-        int nums{};
+        int nums=0;
         for(int i=0;i<n;i++){
             nums=nums^(start+2*i);
         }
