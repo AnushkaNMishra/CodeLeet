91a3b150895b4ee082feddee5eeab9bf1d428983
Anushka N Mishra <84915436+AnushkaNMishra@users.noreply.github.com>
2022-09-25 02:35:47 +0530

    Time: 124 ms (5.17%), Space: 27.3 MB (5.44%) - LeetHub

---
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        unordered_map<int,int>um;
+        for(int i=0;i<nums.size();i++){
+            um[nums[i]]++;
+        }
+        for(auto x: um){
+            if(x.second==1){
+                return x.first;
+            }
+        }
+        return 1;
+    }
+};
