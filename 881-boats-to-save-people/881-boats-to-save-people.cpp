7005d722d1ff13712aad675c951c4b6f7f2ac697
Anushka N Mishra <84915436+AnushkaNMishra@users.noreply.github.com>
2022-09-26 19:09:41 +0530

    Time: 102 ms (78.84%), Space: 42 MB (54.26%) - LeetHub

---
+++ 881-boats-to-save-people/881-boats-to-save-people.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& nums, int limit) {
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
+        int i=0;
+        int j=n-1;
+        int count=0;
+        while(i<=j){
+            if((nums[i]+nums[j])<=limit){
+                count++;
+                i++;
+                j--;
+            }
+            else {
+                count++;
+                j--;
+                
+            }
+            
+        }
+        return count;
+    }
+};
