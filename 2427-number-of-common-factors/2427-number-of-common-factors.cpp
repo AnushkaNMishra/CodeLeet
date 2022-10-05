d0da7fcd1d0da55f5523a94c1aa85fc1deaea9aa
Anushka N Mishra <84915436+AnushkaNMishra@users.noreply.github.com>
2022-10-05 18:32:08 +0530

    Time: 6 ms (7.50%), Space: 6 MB (33.04%) - LeetHub

---
+++ 2427-number-of-common-factors/2427-number-of-common-factors.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int commonFactors(int a, int b) {
+        int count=1;
+        int min=b;
+        if(a<b){
+            min=a;
+        }
+        
+        if(a==0 || b==0)return 0;
+        for(int i=2 ; i <= min; i++){
+                if(a%i==0 && b%i==0){
+                count++;
+            
+            }
+        }
+        return count;
+    }
+};
