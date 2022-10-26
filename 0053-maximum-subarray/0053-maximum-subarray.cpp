9e91a143d0ffc5816fc1653677644aaff1f7dd33
Anushka N Mishra <84915436+AnushkaNMishra@users.noreply.github.com>
2022-10-27 01:32:01 +0530

    Time: 377 ms (5.05%), Space: 67.8 MB (53.74%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int n=nums.size();
+        int maxi=INT_MIN;
+        int curr=0;
+        for(int i=0;i<n;i++){
+            curr=max(nums[i],curr+nums[i]);
+            maxi=max(curr,maxi);
+        }
+        return maxi;
+    }
+};
+
+//kadane's algorithm
