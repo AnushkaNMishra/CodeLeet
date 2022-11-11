3ab402c6e9872685ce5d8fb76eb592a8551853d0
Anushka N Mishra <84915436+AnushkaNMishra@users.noreply.github.com>
2022-11-12 03:23:57 +0530

    Time: 435 ms (5.18%), Space: 27.1 MB (10.78%) - LeetHub

---
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string removeStars(string s) {
+        int n=s.length();
+        string res="";
+        stack<char>st;
+        for(int i=0;i<n;i++){
+            if(st.empty()){
+                st.push(s[i]);
+            }
+            else if(s[i]=='*'){
+                st.pop();
+            }
+            else{
+                st.push(s[i]);
+            }
+        }
+        while(!st.empty()){
+            res+=st.top();
+            st.pop();
+        }
+        reverse(res.begin(),res.end());
+        return res;
+    }
+};
