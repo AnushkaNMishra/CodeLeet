c5d23215b357283e33e965625e540b6b98470601
Anushka N Mishra <84915436+AnushkaNMishra@users.noreply.github.com>
2023-01-22 01:59:34 +0530

    Time: 9 ms (15.04%), Space: 11.6 MB (5.12%) - LeetHub

--- 0093-restore-ip-addresses/0093-restore-ip-addresses.cpp
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.cpp
@@ -1,50 +1,40 @@
 class Solution {
 public: 
     int n;
-    vector<string>ans;
-    
-    bool isvalid(string str)
-    {
+    vector<string>res;
+    bool isvalid(string str){
         if(str[0]=='0')return false;
-        
-        int val=stoi(str);
-        if(val>255)
-        {return false;
-        }
+        int a=stoi(str);
+        if(a>255)return false;
         return true;
     }
     
-    void solve(string& s, int index, int parts, string curr)
+    void helper(string& s, int index, int part,string curr)
     {
-        if(index==n && parts==4){
-            // curr.pop_back(); //removing the last dot  
-            // ans.push_back(curr);
-            
-            // or we to remove the dot we cam simple do
-             ans.push_back(curr.substr(0, curr.length()-1));
-            return;
+        if(index==n && part==4)
+        {
+            res.push_back(curr.substr(0,curr.length()-1));
+            return ;
         }
         if(index+1<=n){
-        solve(s,index+1,parts+1, curr+s.substr(index,1)+".");
+            helper(s, index+1,part+1,curr+s.substr(index,1)+".");
         }
         if(index+2<=n && isvalid(s.substr(index,2))){
-        solve(s,index+2,parts+1, curr+s.substr(index,2)+".");
+            helper(s, index+2,part+1,curr+s.substr(index,2)+".");
         }
         if(index+3<=n && isvalid(s.substr(index,3))){
-        solve(s,index+3,parts+1, curr+s.substr(index,3)+".");
+            helper(s, index+3,part+1,curr+s.substr(index,3)+".");
         }
-
     }
-        
-        
+    
     vector<string> restoreIpAddresses(string s) {
-        ans.clear();
+       res.clear();
         n=s.length();
-        if(n>12)return ans;
+        if(n>12)return res;
+        int part=0;
         string curr="";
-        int parts=0;
-        solve(s,0,parts,curr);  
-        return ans;
+        helper(s,0,part,curr);
+        return res;
     }
 };
 //TC- O(Mn.N)(where n is in the powerand represents the height)(M=No of digits)
