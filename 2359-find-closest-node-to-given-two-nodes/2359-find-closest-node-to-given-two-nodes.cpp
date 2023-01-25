0fbf7496aaed2d11da04ebe7ad60f45c65bb171e
Anushka N Mishra <84915436+AnushkaNMishra@users.noreply.github.com>
2023-01-25 22:18:00 +0530

    Time: 239 ms (63.16%), Space: 97.8 MB (72.85%) - LeetHub

--- 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.cpp
+++ 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.cpp
@@ -1,43 +1,39 @@
 class Solution {
 public:
-    int n;
-    
-    void dfs(vector<int>& edges, int node,vector<int>&dist,vector<bool>&visited){
+   
+    void dfs(vector<int>& edges, int node,vector<int>&dist, vector<bool>&visited){
         visited[node]=true;
-        int v= edges[node];
-        
-        if(v!=-1 && !visited[v]){
-            //visited[v]=true;
-            dist[v]=1+dist[node];
-            dfs(edges,v,dist,visited);
-        }
-        
+        int val=edges[node];
+        if(val!=-1 && !visited[val]){
+            dist[val]=1+dist[node];
+            dfs(edges,val,dist,visited);
+        }   
     }
     
-    
     int closestMeetingNode(vector<int>& edges, int node1, int node2) {
-     n=edges.size();
+        int n=edges.size();
         vector<int>dist1(n,INT_MAX);
         vector<int>dist2(n,INT_MAX);
+        
         vector<bool>visited1(n,false);
         vector<bool>visited2(n,false);
-        //we will call 2 times dfs
+        
         dist1[node1]=0;
         dist2[node2]=0;
         
         dfs(edges,node1,dist1,visited1);
         dfs(edges,node2,dist2,visited2);
         
-        int min_dist_node=-1;
+        int min_node=-1;
         int min_dist=INT_MAX;
         for(int i=0;i<n;i++){
-            int maxd=max(dist1[i],dist2[i]);
-            if(min_dist>maxd){
-                min_dist=maxd;
-                min_dist_node=i;
+            int path=max(dist1[i],dist2[i]);
+            if(min_dist>path){
+                min_dist=path;
+                min_node=i;
             }
         }
-        return min_dist_node;
+        return min_node;
         
     }
 };
