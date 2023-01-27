12756e9ac0ddba6086b0ad7ceb2686f3f944d672
Anushka N Mishra <84915436+AnushkaNMishra@users.noreply.github.com>
2023-01-27 18:17:03 +0530

    Time: 551 ms (25.33%), Space: 38.8 MB (96.46%) - LeetHub

---
+++ 0472-concatenated-words/0472-concatenated-words.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    
+    bool isconcat(string word, unordered_set<string>& st){
+        for(int i=0;i<word.size();i++){
+            string prefix=word.substr(0,i+1);
+            string suffix=word.substr(i+1);
+            
+            if((st.find(prefix)!=st.end() && isconcat(suffix,st)) || (st.find(prefix)!=st.end() && st.find(suffix)!=st.end())){
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    vector<string> findAllConcatenatedWordsInADict(vector<string>& words) {
+        int n=words.size();
+        vector<string>ans;
+        unordered_set<string>st(begin(words),end(words));
+        
+        for(int i=0;i<n;i++){
+            string word=words[i];
+            if(isconcat(word,st)){
+                ans.push_back(word);
+            }
+        }
+        return ans;
+    }
+};
