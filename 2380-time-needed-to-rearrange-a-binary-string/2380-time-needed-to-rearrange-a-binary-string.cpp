c2d54fd9a1c180474223d830f32035d9c743df96
Anushka N Mishra <84915436+AnushkaNMishra@users.noreply.github.com>
2023-02-21 01:51:31 +0530

    Time: 69 ms (67.16%), Space: 6.7 MB (67.80%) - LeetHub

--- 2380-time-needed-to-rearrange-a-binary-string/2380-time-needed-to-rearrange-a-binary-string.cpp
+++ 2380-time-needed-to-rearrange-a-binary-string/2380-time-needed-to-rearrange-a-binary-string.cpp
@@ -3,29 +3,24 @@ public:
     int secondsToRemoveOccurrences(string s) {
         int count=0;
         int n=s.size();
-        
-        if(n==1) return 0;
-        
         while(true){
             bool flag=false;
             int i=1;
-            while(i<n)
-            {
-            
-            if(s[i-1]=='0' && s[i]=='1'){
-                swap(s[i-1],s[i]);
-                flag=true;
-                i+=2;
-            }
-            else{
-                i++;
+            while(i<n){
+                if(s[i-1]=='0' && s[i]=='1'){
+                    swap(s[i-1],s[i]);
+                    flag=true;
+                    i+=2;
+                    
+                }
+                else{
+                    i++;
                 }
             }
             if(flag==false){
                 break;
             }
             count++;
-           // cout<<s<<" "<<count<<endl;
         }
         return count;
     }
