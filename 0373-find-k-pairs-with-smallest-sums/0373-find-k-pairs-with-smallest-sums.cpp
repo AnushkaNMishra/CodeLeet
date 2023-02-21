40c4ee1d265910e3dd07a75fbf92907093a8a799
Anushka N Mishra <84915436+AnushkaNMishra@users.noreply.github.com>
2023-02-21 22:43:45 +0530

    Time: 260 ms (90.52%), Space: 106.3 MB (97.17%) - LeetHub

---
+++ 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
+        vector<vector<int>>res;
+        priority_queue<tuple<int,int,int>>pq;
+        int n=nums1.size(), m=nums2.size();
+        if(n==0 || m==0) return res;
+        
+        pq.push({-nums1[0]-nums2[0],0,0});
+        
+        while(pq.size()&& res.size()<k)
+        {
+            auto first=pq.top();
+            int i=get<1>(first);
+            int j=get<2>(first);
+            pq.pop();
+            res.push_back({nums1[i],nums2[j]});
+            
+            if(i+1<n && j==0){
+                pq.push({-nums1[i+1]-nums2[j],i+1,j});
+            }
+            if(i<n && j+1<m){
+                pq.push({-nums1[i]-nums2[j+1],i,j+1});
+            }
+        }
+        return (res);
+        
+    }
+};
+
+//https://leetcode.com/problems/find-k-pairs-with-smallest-sums/discuss/84551/simple-Java-O(KlogK)-solution-with-explanation
