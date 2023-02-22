6152310c29c891988beffd99eebb0beaf5a00605
Anushka N Mishra <84915436+AnushkaNMishra@users.noreply.github.com>
2023-02-23 02:02:03 +0530

    Time: 10 ms (94.81%), Space: 10.3 MB (56.36%) - LeetHub

--- 0506-relative-ranks/0506-relative-ranks.cpp
+++ 0506-relative-ranks/0506-relative-ranks.cpp
@@ -1,31 +1,31 @@
 class Solution {
 public:
     vector<string> findRelativeRanks(vector<int>& score) {
-        int n=score.size();
-        vector<string>ans(n);
+       int n=score.size();
+        vector<string>v(n);
         priority_queue<pair<int,int>>pq;
         for(int i=0;i<n;i++){
             pq.push({score[i],i});
         }
         int i=1;
         while(!pq.empty()){
+            
             int topp=pq.top().second;
             if(i==1){
-                ans[topp]="Gold Medal";
+                v[topp]="Gold Medal";
             }
             else if(i==2){
-                ans[topp]="Silver Medal";
+                v[topp]="Silver Medal";
             }
             else if(i==3){
-                ans[topp]="Bronze Medal";
+                v[topp]="Bronze Medal";
             }
             else{
-                ans[topp]=to_string(i);
+                v[topp]=to_string(i);
             }
             i++;
             pq.pop();
         }
-        return ans;
-        
+        return v;
     }
 };
