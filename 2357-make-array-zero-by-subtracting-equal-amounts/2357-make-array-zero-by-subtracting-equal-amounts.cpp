c8512e15b3740c9879c1648a692c4ed1295fb01a
Anushka N Mishra <84915436+AnushkaNMishra@users.noreply.github.com>
2023-03-01 04:11:59 +0530

    Time: 6 ms (28.67%), Space: 8.3 MB (85.58%) - LeetHub

--- 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.cpp
+++ 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.cpp
@@ -1,12 +1,31 @@
 class Solution {
 public:
     int minimumOperations(vector<int>& nums) {
-       set<int>s;
-        for(auto it:nums){
-            if(it!=0){
-                s.insert(it);
+       // set<int>s;
+       //  for(auto it:nums){
+       //      if(it!=0){
+       //          s.insert(it);
+       //      }
+       //  }
+       //  return s.size();
+        
+        priority_queue<int>pq;
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]!=0){
+                if(pq.empty()){
+                    pq.push(nums[i]);
+                }
+                else{
+                    if(pq.top()==nums[i]){
+                        continue;
+                    }
+                    else{
+                        pq.push(nums[i]);
+                    }
+                }
             }
         }
-        return s.size();
+        return pq.size();
     }
 };
