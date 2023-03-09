324d814a956de6b5182901ce9e56fb4023747b64
Anushka N Mishra <84915436+AnushkaNMishra@users.noreply.github.com>
2023-03-09 16:38:23 +0530

    Time: 8 ms (76.50%), Space: 7.6 MB (68.59%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -9,15 +9,38 @@
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
-        unordered_map<ListNode*,int>mp;
-        ListNode* curr=head;
-        while(curr!=NULL){
-            if(mp[curr]>1){
-                return curr;
+        ListNode *slow=head,*fast=head;
+        while(fast!=NULL && fast->next!=NULL){
+            slow=slow->next;
+            fast=fast->next->next;
+            if(slow==fast){
+                break;
             }
-            mp[curr]++;
-            curr=curr->next;
         }
-        return NULL;
-    }
+        if(!(fast && fast->next)){
+            return NULL;
+        }
+        while(head!=slow){
+            head=head->next;
+            slow=slow->next;
+            }
+        return head;
+        }
 };
+
+
+// class Solution {
+// public:
+//     ListNode *detectCycle(ListNode *head) {
+//         unordered_map<ListNode*,int>mp;
+//         ListNode* curr=head;
+//         while(curr!=NULL){
+//             if(mp[curr]>1){
+//                 return curr;
+//             }
+//             mp[curr]++;
+//             curr=curr->next;
+//         }
+//         return NULL;
+//     }
+// };
