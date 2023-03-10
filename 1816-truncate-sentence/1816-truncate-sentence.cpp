344de95c7dfe9841809713015fcc464da5ad8dce
Anushka N Mishra <84915436+AnushkaNMishra@users.noreply.github.com>
2023-03-11 00:31:09 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (26.92%) - LeetHub

--- 1816-truncate-sentence/1816-truncate-sentence.cpp
+++ 1816-truncate-sentence/1816-truncate-sentence.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     string truncateSentence(string s, int k) {
-        stack<string>st;
         string ans="";
         for(int i=0;i<s.size();i++){
             string temp="";
@@ -11,20 +10,12 @@ public:
                 temp+=s[i];
                 i++;
             }
-            //st.push(temp);
             ans+=temp;
             ans+=" ";
             k--;
         }
         s.erase();
-        
-        // while(!st.empty()){
-        //     ans+=st.top();
-        //     st.pop();
-        //     ans+=" ";
-        // }
         ans.pop_back();
-        //strcpy(s,ans);
         s=ans;
         
         return s;
