acd56d804d57332fa70f5917d31b5b4ab8d43935
Anushka N Mishra <84915436+AnushkaNMishra@users.noreply.github.com>
2023-03-10 16:30:05 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (31.69%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
@@ -1,16 +1,30 @@
 class Solution {
 public:
     bool checkIfPangram(string sentence) {
-        unordered_map<char,int>mp;
-        vector<int>v(26);
+        unordered_set<char>s;
         for(auto it:sentence){
-            v[it-'a']++;
+            s.insert(it);
         }
-        for(auto it:v){
-            if(it==0){
-                return false;
-            }
-        }
+        if(s.size()==26){
         return true;
+        }
+        return false;
     }
 };
+
+// class Solution {
+// public:
+//     bool checkIfPangram(string sentence) {
+//         unordered_map<char,int>mp;
+//         vector<int>v(26);
+//         for(auto it:sentence){
+//             v[it-'a']++;
+//         }
+//         for(auto it:v){
+//             if(it==0){
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
+// };
