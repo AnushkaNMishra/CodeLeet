78502d38ef75118cee22c1142f0c43f7e4c93916
Anushka N Mishra <84915436+AnushkaNMishra@users.noreply.github.com>
2023-03-12 03:04:40 +0530

    Time: 3 ms (67.02%), Space: 6.6 MB (64.19%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.cpp
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.cpp
@@ -2,24 +2,25 @@ class Solution {
 public:
     bool halvesAreAlike(string s) {
         string s2="";
-        int n2=s.size()/2;
-        for(int i=0;i<=n2;i++){
-            s2+=s[i];
-        }
-        
-        int c1=0,c2=0;
-       for(int i=n2;i<s.size();i++){
-            if(s[i]=='A' || s[i]=='a' || s[i]=='E' || s[i]=='e' || s[i]=='I' || s[i]=='i' || s[i]=='O' || s[i]=='o' || s[i]=='U' || s[i]=='u'){
+        int c1=0;
+        int n=s.size();
+        for(int i=0;i<n;i++){
+            if(i<n/2){
+                if(s[i]=='A' || s[i]=='a' || s[i]=='E' || s[i]=='e' || s[i]=='I' || s[i]=='i' || s[i]=='O' || s[i]=='o' || s[i]=='U' || s[i]=='u'){
                 c1++;
+                }
             }
-        }
-        for(int i=0;i<n2;i++){
-           if(s2[i]=='A' || s2[i]=='a' || s2[i]=='E' || s2[i]=='e' || s2[i]=='I' || s2[i]=='i' || s2[i]=='O' || s2[i]=='o' || s2[i]=='U' || s2[i]=='u'){
-                c2++;
+            else{
+                if(s[i]=='A' || s[i]=='a' || s[i]=='E' || s[i]=='e' || s[i]=='I' || s[i]=='i' || s[i]=='O' || s[i]=='o' || s[i]=='U' || s[i]=='u'){
+                c1--;
+                }
+                
             }
+             
         }
         
-        if(c1==c2){
+      
+        if(c1==0){
             return true;
         }
         return false;
