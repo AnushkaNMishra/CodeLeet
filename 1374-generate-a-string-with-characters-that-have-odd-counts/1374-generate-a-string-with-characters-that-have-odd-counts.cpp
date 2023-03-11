7c1a2e831f19565b7af840cdf2ad5c462aa94328
Anushka N Mishra <84915436+AnushkaNMishra@users.noreply.github.com>
2023-03-12 03:15:02 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (30.57%) - LeetHub

--- 1374-generate-a-string-with-characters-that-have-odd-counts/1374-generate-a-string-with-characters-that-have-odd-counts.cpp
+++ 1374-generate-a-string-with-characters-that-have-odd-counts/1374-generate-a-string-with-characters-that-have-odd-counts.cpp
@@ -3,13 +3,13 @@ public:
     string generateTheString(int n) {
         string s="";
         if(n%2==0){
-            for(int i=1;i<=n-1;i++){
+            for(int i=0;i<n-1;i++){
                 s.push_back('a');
             }
             s.push_back('b');
         }
         else{
-            for(int i=1;i<=n;i++){
+            for(int i=0;i<n;i++){
                 
             s.push_back('a');
             }
