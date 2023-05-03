17a8bff9cdde07441400ab96d938b5842b474946
Anushka N Mishra <84915436+AnushkaNMishra@users.noreply.github.com>
2023-05-03 20:30:11 +0530

    Time: 64 ms (17.47%), Space: 41.8 MB (69.44%) - LeetHub

--- 2620-counter/2620-counter.js
+++ 2620-counter/2620-counter.js
@@ -3,8 +3,10 @@
  * @return {Function} counter
  */
 var createCounter = function(n) {
+    let count=n-1;
     return function() {
-        return n++;
+        count++;
+        return count;
     };
 };
 
