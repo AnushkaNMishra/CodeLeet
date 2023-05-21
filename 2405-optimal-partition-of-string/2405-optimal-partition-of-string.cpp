43ff86a963d85522c2d8bd50c41b318b587cb385
Anushka N Mishra <84915436+AnushkaNMishra@users.noreply.github.com>
2023-05-22 03:27:17 +0530

    Time: 265 ms (11.99%), Space: 71.9 MB (17.26%) - LeetHub

---
+++ 2405-optimal-partition-of-string/2405-optimal-partition-of-string.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int partitionString(string s) {
+        set<char>st;
+        int count=0;
+        for(int i=0;i<s.size();i++){
+            while(i<s.size() && st.find(s[i])==st.end()){
+                st.insert(s[i]);
+                i++;
+            }
+            i--;
+            count++;
+            st.clear();
+        }
+        return count;
+    }
+};
