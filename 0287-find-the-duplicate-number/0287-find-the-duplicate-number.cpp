01c110ce7b64e6c2cda991a9d469c65329c79782
Anushka N Mishra <84915436+AnushkaNMishra@users.noreply.github.com>
2023-05-22 03:12:46 +0530

    Time: 301 ms (8.20%), Space: 100.7 MB (10.39%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -3,8 +3,8 @@ public:
     int findDuplicate(vector<int>& nums) {
         int n=nums.size();
         unordered_map<int,int>mp;
-        for(int i=0;i<n;i++){
-            mp[nums[i]]++;
+        for(auto it:nums){
+            mp[it]++;
         }
         for(auto it: mp){
             if(it.second>1){
