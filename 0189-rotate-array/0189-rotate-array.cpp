9fcfa6e46cbadcc474e85eb45550abdd113858ba
Anushka N Mishra <84915436+AnushkaNMishra@users.noreply.github.com>
2023-07-13 04:11:59 +0530

    Time: 41 ms (19.86%), Space: 24.8 MB (87.40%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    
+    void func(vector<int>&nums, int i, int j){
+        int a=i;
+        int b=j;
+        
+        while(a<b){
+            int temp=nums[a];
+            nums[a]=nums[b];
+            nums[b]=temp;
+            a++;
+            b--;
+            
+        }
+    }
+    
+    void rotate(vector<int>& nums, int k) {
+       
+       int n=nums.size();
+        k=k%n;
+        if(k<0){
+            k+=n;
+        }
+        func(nums,0,n-k-1);
+        func(nums,n-k,n-1);
+        func(nums,0,n-1);
+    }
+};
