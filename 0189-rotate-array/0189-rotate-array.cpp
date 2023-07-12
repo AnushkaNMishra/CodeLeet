f8eb576affddd01612b96f5b531904452c255c97
Anushka N Mishra <84915436+AnushkaNMishra@users.noreply.github.com>
2023-07-13 04:13:12 +0530

    Time: 29 ms (88.65%), Space: 24.9 MB (64.07%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -6,9 +6,7 @@ public:
         int b=j;
         
         while(a<b){
-            int temp=nums[a];
-            nums[a]=nums[b];
-            nums[b]=temp;
+            swap(nums[a],nums[b]);
             a++;
             b--;
             
