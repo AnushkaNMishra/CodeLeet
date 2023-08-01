3a0fab9bf7123ef8082756860f6c8e04dc91ec73
Anushka N Mishra <84915436+AnushkaNMishra@users.noreply.github.com>
2023-08-02 03:12:25 +0530

    Time: 110 ms (84.29%), Space: 57.6 MB (44.95%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,34 +1,37 @@
 class Solution {
 public:
     
-    
-    void helper(string& s, int index, vector<string>&v,vector<vector<string>>& ans){
-        if(index==s.length()){
-            ans.push_back(v);
-            return;
-        }
-        for(int i=index;i<s.length();i++){
-            if(ispalindrome(s,index,i)){
-                v.push_back(s.substr(index,i-index+1));
-                helper(s,i+1,v,ans);
-                v.pop_back();
-            }
-        }
-    }
-    
-     bool ispalindrome(string& s, int start,int end){
+    bool ispalindrome(string s, int start,int end){
         while(start<=end){
             if(s[start++]!=s[end--]){
                 return false;
             }
+            
         }
         return true;
     }
     
+    
+    void solve(string s, int ind,vector<vector<string>>&ans,vector<string>&temp){
+        if(ind==s.size()){
+            ans.push_back(temp);
+            return;
+        }
+        
+        for(int i=ind;i<s.size();i++){
+            if(ispalindrome(s,ind,i)){
+                temp.push_back(s.substr(ind,i-ind+1));
+                solve(s,i+1,ans,temp);
+                temp.pop_back();
+            }
+        }
+    }
+    
+    
     vector<vector<string>> partition(string s) {
         vector<vector<string>>ans;
-        vector<string>part;
-        helper(s,0,part,ans);
+        vector<string>temp;
+        solve(s,0,ans,temp);
         return ans;
     }
 };
