c209596bd3a00c6b885c84f5c241f285ed607dd1
Anushka N Mishra <84915436+AnushkaNMishra@users.noreply.github.com>
2023-08-01 10:09:36 +0530

    Time: 26 ms (86.71%), Space: 6.3 MB (61.07%) - LeetHub

---
+++ 0037-sudoku-solver/0037-sudoku-solver.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    void solveSudoku(vector<vector<char>>& board) {
+        solve(board);
+    }
+    
+    bool solve(vector<vector<char>>& board){
+        for(int i=0;i<board.size();i++){
+            for(int j=0;j<board[0].size();j++){
+                if(board[i][j]=='.'){
+                    for(char s='1';s<='9';s++){
+                        if(isvalid(board,i,j,s)){
+                            board[i][j]=s;
+                            
+                            if(solve(board)==true){
+                                return true;
+                            }
+                            else{
+                                board[i][j]='.';
+                            }
+                        }
+                    }
+                     return false;
+                }
+            }
+        }
+    return true;
+    }
+    
+    bool isvalid(vector<vector<char>>& board,int row,int col,char s){
+        for(int i=0;i<9;i++){
+            if(board[row][i]==s){
+                return false;
+            }
+            else if(board[i][col]==s){
+                return false;
+            }
+            else if(board[3*(row/3)+i/3][3*(col/3)+i%3]==s){
+                return false;
+            }
+            
+        }
+        return true;
+    }
+};
