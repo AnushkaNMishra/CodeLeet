0f9167450f6385e3affff3904ba9041103e6e45b
Anushka N Mishra <84915436+AnushkaNMishra@users.noreply.github.com>
2023-08-02 03:19:16 +0530

    Time: 97 ms (95.02%), Space: 57.5 MB (44.95%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -3,16 +3,18 @@ public:
     
     bool ispalindrome(string s, int start,int end){
         while(start<=end){
-            if(s[start++]!=s[end--]){
+            if(s[start]!=s[end]){
                 return false;
             }
-            
+            else{
+                start++;
+                end--;
+            }
         }
         return true;
     }
     
-    
-    void solve(string s, int ind,vector<vector<string>>&ans,vector<string>&temp){
+    void solve(int ind,string s,vector<string>&temp,vector<vector<string>>&ans){
         if(ind==s.size()){
             ans.push_back(temp);
             return;
@@ -21,17 +23,16 @@ public:
         for(int i=ind;i<s.size();i++){
             if(ispalindrome(s,ind,i)){
                 temp.push_back(s.substr(ind,i-ind+1));
-                solve(s,i+1,ans,temp);
+                solve(i+1,s,temp,ans);
                 temp.pop_back();
             }
         }
     }
-    
-    
+  
     vector<vector<string>> partition(string s) {
-        vector<vector<string>>ans;
+       vector<vector<string>>ans;
         vector<string>temp;
-        solve(s,0,ans,temp);
+        solve(0,s,temp,ans);
         return ans;
     }
 };
